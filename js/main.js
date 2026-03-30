2a19aca5cc657dccd7eb6d3f90cb82d77389deaf
mohmed <mohmedkhalad390@gmail.com>
2026-03-30 15:06:19 +0200

    The design has been modified to fit all screen sizes, and the lock icon images have been removed.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-const formWrapper =document.querySelector('.form-wrapper');
+const formWrapper=document.querySelector('.form-wrapper');
 const btnLoginNav=document.getElementById("btn-login-nav");
 const iconClose=document.querySelectorAll("#icon-close");
 const signUpBtnLink= document.querySelector(".signUpBtn-link");
 const signInBtnLink= document.querySelector(".signInBtn-link");
 const formSignUpBtn=document.querySelector(".form-sign-up-btn");
 const formLoginBtn=document.querySelector(".form-login-btn");
-
 const displayFormSignIn=()=> formWrapper.style.display='block' ;    
 const closeFromSignIn=()=> formWrapper.style.display='none';
 const toggleSignIn=(element)=>{
